62fc040c41a97b3e33a0bc2c823b4783a1455c62
Ruixin Lee <41141489+Breeze1in1drizzle@users.noreply.github.com>
2021-10-27 14:53:31 +0800

    updates

---
+++ BreFL/test/ptdist.py
@@ -0,0 +1,21 @@
+# filename 'ptdist.py'
+import torch
+import torch.distributed as dist
+
+
+def main(rank, world):
+    if rank == 0:
+        x = torch.tensor([1., -1.]) # Tensor of interest
+        dist.send(x, dst=1)
+        print('Rank-0 has sent the following tensor to Rank-1')
+        print(x)
+    else:
+        z = torch.tensor([0., 0.]) # A holder for recieving the tensor
+        dist.recv(z, src=0)
+        print('Rank-1 has recieved the following tensor from Rank-0')
+        print(z)
+
+
+if __name__ == '__main__':
+    dist.init_process_group(backend='mpi')
+    main(dist.get_rank(), dist.get_world_size())
